01162afbf3fd977687df9ff64f934a2d67d6c946
Yusuf Eren Yıldız <yusuferen024@gmail.com>
2025-02-27 15:43:29 +0300

    ...

--- CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -8,15 +8,11 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
 {
     public class Order
     {
-
         public int OrderId { get; set; }
-
         public int ProductId { get; set; }
         public Product Product { get; set; }
-
         public int Quantity { get; set; }
         public decimal Price { get; set; }
-
         public decimal TotalPrice { get; set; }
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
