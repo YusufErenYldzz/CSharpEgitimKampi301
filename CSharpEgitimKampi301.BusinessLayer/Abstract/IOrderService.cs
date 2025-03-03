133a9400e19785d3f4dc68b45cccf2bbfd03f87c
Yusuf Eren Yıldız <yusuferen024@gmail.com>
2025-03-03 13:32:14 +0300

    ...

---
+++ CSharpEgitimKampi301.BusinessLayer/Abstract/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+
+namespace CSharpEgitimKampi301.BusinessLayer
+{
+    public interface IOrderService : IGenericService<Order>
+    {
+
+    }
+}
---
+++ CSharpEgitimKampi301.BusinessLayer/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Abstract
+{
+    public interface IProductService : IGenericService<Product>
+    {
+
+
+
+    }
+}
